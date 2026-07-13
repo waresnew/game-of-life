5c6474d1d26f6040254ce248acc529ceeb444f75
newwares <45801973+waresnew@users.noreply.github.com>
2026-07-12 22:20:11 -0400

    scientific notation

--- web/src/lib/components/Stats.svelte
+++ web/src/lib/components/Stats.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import { getPerfStats, uiState, getRenderStats } from '$lib/shared.svelte';
+	function formatBigInt(n: bigint) {
+		const sciNotationThreshold = 9_999_999_999n;
+		if (n > sciNotationThreshold) {
+			return n.toLocaleString('en-US', { notation: 'scientific', maximumFractionDigits: 3 });
+		} else {
+			return n.toString();
+		}
+	}
 </script>
 
 <div class="flex flex-col gap-2 w-full flex-none">
 	<div class="flex gap-8">
-		<span class="min-w-0 flex-1 truncate">Generation: {uiState.generation}</span>
-		<span class="min-w-0 flex-1 truncate">Alive: {getPerfStats().alives}</span>
+		<span class="min-w-0 flex-1 truncate">Generation: {formatBigInt(uiState.generation)}</span>
+		<span class="min-w-0 flex-1 truncate">Alive: {formatBigInt(BigInt(getPerfStats().alives))}</span
+		>
 	</div>
 	<div class="flex gap-8" id="stats-display">
 		<span class="min-w-0 flex-1 truncate">FPS: {uiState.fps}</span>
