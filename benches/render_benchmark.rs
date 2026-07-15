58bffb557525b11fc813f96b54eea43ce9002fe8
newwares <45801973+waresnew@users.noreply.github.com>
2026-07-14 20:11:35 -0400

    adjust benchmark

--- benches/render_benchmark.rs
+++ benches/render_benchmark.rs
@@ -1,22 +1,21 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use game_of_life::renderer::{CELL_SIZE_EXP, CellPoint, Renderer, ScreenPoint, ViewportInfo};
-use num_bigint::BigInt;
+use game_of_life::renderer::{CELL_SIZE_EXP, Renderer, ScreenPoint, ViewportInfo};
 
 fn filled_rect(c: &mut Criterion) {
     let mut renderer = Renderer::new(0);
-    for i in 0..=16 {
-        for j in 0..=16 {
-            renderer.handle_draw(ScreenPoint::new(
-                i * (1 << CELL_SIZE_EXP),
-                j * (1 << CELL_SIZE_EXP),
-            ));
+    renderer.handle_zoom(CELL_SIZE_EXP as i32, ScreenPoint::new(0, 0));
+    const CANVAS_SIZE: i64 = 100;
+    for i in 0..CANVAS_SIZE {
+        for j in 0..CANVAS_SIZE {
+            renderer.handle_draw(ScreenPoint::new(i, j));
         }
     }
     renderer.update_viewport(ViewportInfo {
-        canvas_dims: ScreenPoint::new(1500, 1500),
+        canvas_dims: ScreenPoint::new(CANVAS_SIZE, CANVAS_SIZE),
     });
-    renderer.handle_zoom(-3, ScreenPoint::new(0, 0));
-    c.bench_function("filled 64x64 render", |b| b.iter(|| renderer.render()));
+    c.bench_function("filled 1:1 pixel:cell ratio render", |b| {
+        b.iter(|| renderer.render())
+    });
 }
 
 criterion_group!(benches, filled_rect);
